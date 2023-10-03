41983a7f544b8ae9828cf820cf401a3893f27cf7
sapiens19 <sapiens19@pop-os.localdomain>
2023-10-02 21:19:39 -0300

    Create import categories Use Case and stream concept

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router, response } from "express";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoryController } from "../modules/cars/useCases/listCategories";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
+import multer from 'multer';
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response)
@@ -11,5 +16,9 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
   return listCategoryController.handle(request, response)
 });
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response)
+})
 
 export { categoriesRoutes };
