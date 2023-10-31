9a0835c75a35cd5c865ac9a030f95b75fa1b7523
sapiens19 <sapiens19@pop-os.localdomain>
2023-10-31 19:35:36 -0300

    Created authenticated user middleware to other routes

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+import { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
+import { UsersRepository } from '../modules/accounts/repositories/implementations/usersRepository'
+
+interface IPayload {
+  sub: string
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    throw new Error('Token missing')
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      'f927559c1d62dccb6379da8a743de0ec',
+    ) as IPayload
+    console.log(user_id)
+
+    const usersRepository = new UsersRepository()
+    const user = await usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new Error('User does not exists!')
+    }
+    next()
+  } catch {
+    throw new Error('Invalid token!')
+  }
+}
--- src/routes/specificationRoutes.ts
+++ src/routes/specificationRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/createSpecificationController'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 
 const specificationRoutes = Router()
 
 const createSpecificationController = new CreateSpecificationController()
 
+specificationRoutes.use(ensureAuthenticated)
 specificationRoutes.post('/', createSpecificationController.handle)
 
 export { specificationRoutes }
