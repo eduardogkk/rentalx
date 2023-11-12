5e07f9d50c5db857209f9f089db937a71ca592b3
sapiens19 <sapiens19@pop-os.localdomain>
2023-11-12 20:28:42 -0300

    Added verification about Avatar if already exist and delete this avatar

---
+++ src/utils/file.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-useless-return */
+import fs from 'fs'
+
+export const deleteFile = async (filename: string) => {
+  try {
+    await fs.promises.stat(filename)
+  } catch {
+    return
+  }
+  await fs.promises.unlink(filename)
+}
