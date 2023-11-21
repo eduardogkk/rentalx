31ce4caff8de7453095298d2a5c096c82e3d28c0
sapiens19 <sapiens19@pop-os.localdomain>
2023-11-21 09:57:59 -0300

    Created authenticate user test

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+import { User } from '../../entities/Users'
+import { ICreateUsersDTO, IUsersRepository } from '../IUsersRepository'
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = []
+
+  async create({
+    name,
+    driver_license,
+    password,
+    email,
+  }: ICreateUsersDTO): Promise<void> {
+    const user = new User()
+
+    Object.assign(user, {
+      name,
+      driver_license,
+      password,
+      email,
+    })
+
+    this.users.push(user)
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email)
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id)
+  }
+}
+
+export { UsersRepositoryInMemory }
