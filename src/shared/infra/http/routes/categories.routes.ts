617ae320021106bc29e38649c2ad70a5b2e55244
sapiens19 <sapiens19@pop-os.localdomain>
2023-11-27 19:09:41 -0300

    Created 'is admin' middleware and add in application

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from 'multer'
 import { CreateCategoryController } from '../../../../modules/cars/useCases/createCategory/createCategoryController'
 import { ImportCategoryController } from '../../../../modules/cars/useCases/importCategory/importCategoryController'
 import { ListCategoryController } from '../../../../modules/cars/useCases/listCategories/listCategoryController'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+import { ensureAdmin } from '../middlewares/ensureAdmin'
 
 const categoriesRoutes = Router()
 const upload = multer({
@@ -14,13 +16,20 @@ const createCategoryController = new CreateCategoryController()
 const importCategoryController = new ImportCategoryController()
 const listCategoryController = new ListCategoryController()
 
-categoriesRoutes.post('/', createCategoryController.handle)
+categoriesRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle,
+)
 
 categoriesRoutes.get('/', listCategoryController.handle)
 
 categoriesRoutes.post(
   '/import',
   upload.single('file'),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle,
 )
 
