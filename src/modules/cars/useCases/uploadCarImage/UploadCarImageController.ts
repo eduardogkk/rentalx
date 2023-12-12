380b3ecead3c788dd2457b1aba3b0904166f1292
sapiens19 <sapiens19@pop-os.localdomain>
2023-12-12 12:03:10 -0300

    Created use case and car images controller

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
@@ -1,3 +1,28 @@
-class UploadCarImageController { }
+/* eslint-disable camelcase */
+import { Request, Response } from 'express'
+import { UploadCarImageUseCase } from './UploadCarImageUseCase'
+import { container } from 'tsyringe'
+
+interface IFiles {
+  filename: string
+}
+
+class UploadCarImageController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params
+    const images = request.files as IFiles[]
+
+    const uploadCarImageUseCase = container.resolve(UploadCarImageUseCase)
+
+    const images_name = images.map((file) => file.filename)
+
+    await uploadCarImageUseCase.execute({
+      car_id: id,
+      images_name,
+    })
+
+    return response.status(201).send()
+  }
+}
 
 export { UploadCarImageController }
--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,3 +1,26 @@
-class UploadCarImageUseCase { }
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-useless-constructor */
+/* eslint-disable camelcase */
+import { inject, injectable } from 'tsyringe'
+import { ICarsImagesRepository } from '../../repositories/ICarsImagesRepository'
+
+interface IRequest {
+  car_id: string
+  images_name: string[]
+}
+
+@injectable()
+class UploadCarImageUseCase {
+  constructor(
+    @inject('CarsImagesRepository')
+    private carsImagesRepository: ICarsImagesRepository,
+  ) { }
+
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    images_name.map(async (image) => {
+      await this.carsImagesRepository.create(car_id, image)
+    })
+  }
+}
 
 export { UploadCarImageUseCase }
