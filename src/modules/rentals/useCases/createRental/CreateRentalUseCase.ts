e90c207cc03d8a4502b83b77386c9eab63ad3924
sapiens19 <sapiens19@pop-os.localdomain>
2024-01-02 20:11:18 -0300

    created date provider

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,13 +1,10 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-useless-constructor */
 /* eslint-disable camelcase */
-import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
+import { IDateProvider } from '../../../../shared/container/providers/DateProvider/IDateProvider'
 import { AppError } from '../../../../shared/errors/AppError'
 import { Rental } from '../../infra/typeorm/entities/Rental'
 import { IRentalsRepository } from '../../repositories/IRentalsRepository'
-
-dayjs.extend(utc)
 
 interface IRequest {
   user_id: string
@@ -16,7 +13,10 @@ interface IRequest {
 }
 
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) { }
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider
+  ) { }
 
   async execute({
     car_id,
@@ -41,9 +41,9 @@ class CreateRentalUseCase {
       throw new AppError("There's a rental in progress for user!")
     }
 
-    const expectedReturnDateFormat = dayjs(expected_return_date).utc().local().format()
-    const dateNow = dayjs().utc().local().format()
-    const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours")
+    const dateNow = this.dateProvider.dateNow()
+
+    const compare = this.dateProvider.compareInHours(dateNow, expected_return_date)
 
     if (compare < minimumHour) {
       throw new AppError('Invalid return time!')
