2342a84c88bba3051dda5211104035d3ccf02fcc
sapiens19 <sapiens19@pop-os.localdomain>
2024-02-14 17:27:17 -0300

    Created Mail Provider

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -4,6 +4,7 @@ import { IUserTokensRepository } from "../../repositories/IUserTokensRepository"
 import { AppError } from "../../../../shared/errors/AppError";
 import { v4 as uuidV4 } from "uuid";
 import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "../../../../shared/container/providers/MailProvider/IMailProvider";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
@@ -15,7 +16,10 @@ class SendForgotPasswordMailUseCase {
     private userTokensRepository: IUserTokensRepository,
 
     @inject('DayjsDateProvider')
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+
+    @inject('EtherealMailProvider')
+    private mailProvider: IMailProvider
   ) { }
 
   async execute(email: string) {
@@ -34,6 +38,8 @@ class SendForgotPasswordMailUseCase {
       user_id: user.id,
       expires_date,
     })
+
+    await this.mailProvider.sendMail(email, "recuperação de senha", `O link para o reset é ${token}`)
   }
 }
 
