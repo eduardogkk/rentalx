83d237924b664d74e574d6ec76bafd7e3b383317
sapiens19 <sapiens19@pop-os.localdomain>
2024-02-15 20:06:04 -0300

    Created reset password user use case and controller

--- src/modules/accounts/infra/typeorm/repositories/UserTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UserTokensRepository.ts
@@ -22,14 +22,20 @@ class UserTokensRepository implements IUserTokensRepository {
     return userTokens
   }
 
-  findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens> {
-    const userTokens = this.repository.findOne({ user_id, refresh_token })
+  async findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens> {
+    const userTokens = await this.repository.findOne({ user_id, refresh_token })
 
     return userTokens
   }
 
   async deleteById(id: string): Promise<void> {
     await this.repository.delete(id)
+  }
+
+  async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    const userToken = await this.repository.findOne({ refresh_token })
+
+    return userToken
   }
 }
 
---
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserUseCase.ts
@@ -0,0 +1,47 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IUserTokensRepository } from "../../repositories/IUserTokensRepository";
+import { AppError } from "../../../../shared/errors/AppError";
+import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
+import { hash } from "bcryptjs";
+
+interface IRequest {
+  token: string,
+  password: string
+}
+
+@injectable()
+class ResetPasswordUserUseCase {
+  constructor(
+    @inject("UserTokensRepository")
+    private userTokensRepository: IUserTokensRepository,
+
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ token, password }: IRequest): Promise<void> {
+    const userToken = await this.userTokensRepository.findByRefreshToken(token)
+
+    if (!userToken) {
+      throw new AppError('Token invalid!')
+    }
+
+    if (this.dateProvider.compareIfBefore(userToken.expires_date, this.dateProvider.dateNow())) {
+      throw new AppError('Token expires!')
+    }
+
+    const user = await this.usersRepository.findById(userToken.id)
+
+    user.password = await hash(password, 8)
+
+    await this.usersRepository.create(user)
+
+    await this.userTokensRepository.deleteById(userToken.id)
+  }
+}
+
+export { ResetPasswordUserUseCase }
