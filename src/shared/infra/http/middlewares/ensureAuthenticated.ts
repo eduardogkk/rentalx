e59e9ddfa11433e28d422aaa0f901176e7ef647d
sapiens19 <sapiens19@pop-os.localdomain>
2024-02-22 14:38:23 -0300

    Att refresh_token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -17,8 +17,6 @@ export async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization
 
-  const userTokensRepository = new UserTokensRepository()
-
   if (!authHeader) {
     throw new AppError('Token missing', 401)
   }
@@ -28,15 +26,9 @@ export async function ensureAuthenticated(
   try {
     const { sub: user_id } = verify(
       token,
-      auth.secret_refresh_token
+      auth.secret_token
     ) as IPayload
     console.log(user_id)
-
-    const user = await userTokensRepository.findByUserIdAndRefreshToken(user_id, token)
-
-    if (!user) {
-      throw new AppError('User does not exists!', 401)
-    }
 
     request.user = {
       id: user_id,
