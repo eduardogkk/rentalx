4e6afde7d1d93423791ae0f80e2109667b3fa16f
sapiens19 <sapiens19@pop-os.localdomain>
2024-03-08 16:03:48 -0300

    Created Upload provider

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -3,7 +3,7 @@
 /* eslint-disable camelcase */
 import { inject, injectable } from 'tsyringe'
 import { IUsersRepository } from '../../repositories/IUsersRepository'
-import { deleteFile } from '../../../../utils/file'
+import { IStorageProvider } from '../../../../shared/container/providers/StorageProvider/IStorageProvider'
 
 interface IRequest {
   user_id: string
@@ -15,12 +15,16 @@ class UpdateUserAvatarUseCase {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) { }
 
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id)
 
-    if (user.avatar) { await deleteFile(`./tmp/avatar/${user.avatar}`) }
+    if (user.avatar) { await this.storageProvider.delete(user.avatar, "avatar") }
+
+    await this.storageProvider.save(avatar_file, "avatar")
 
     user.avatar = avatar_file
     await this.usersRepository.create(user)
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { DayjsDateProvider } from './DateProvider/implementations/DayjsDateProvi
 import { IDateProvider } from './DateProvider/IDateProvider'
 import { IMailProvider } from './MailProvider/IMailProvider'
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider'
+import { LocalStorageProvider } from './StorageProvider/implementations/LocalStoregeProvider'
+import { IStorageProvider } from './StorageProvider/IStorageProvider'
 
 container.registerSingleton<IDateProvider>(
   'DayjsDateProvider',
@@ -12,4 +14,9 @@ container.registerSingleton<IDateProvider>(
 container.registerInstance<IMailProvider>(
   'EtherealMailProvider',
   new EtherealMailProvider(),
+)
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  LocalStorageProvider
 )
