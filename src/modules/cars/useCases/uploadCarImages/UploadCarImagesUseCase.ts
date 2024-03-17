c2332c0506af01b948139d25741129d97e718443
sapiens19 <sapiens19@pop-os.localdomain>
2024-03-17 19:33:07 -0300

    Added upload provider in upload car images

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -3,6 +3,7 @@
 /* eslint-disable camelcase */
 import { inject, injectable } from 'tsyringe'
 import { ICarsImagesRepository } from '../../repositories/ICarsImagesRepository'
+import { IStorageProvider } from '../../../../shared/container/providers/StorageProvider/IStorageProvider'
 
 interface IRequest {
   car_id: string
@@ -14,11 +15,15 @@ class UploadCarImagesUseCase {
   constructor(
     @inject('CarsImagesRepository')
     private carsImagesRepository: ICarsImagesRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) { }
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image) => {
       await this.carsImagesRepository.create(car_id, image)
+      await this.storageProvider.save(image, "cars")
     })
   }
 }
