6b38b4445cc13aab920eff29442cc03180ce833c
sapiens19 <sapiens19@pop-os.localdomain>
2024-03-17 21:27:36 -0300

    Created URL of avatar access

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,6 +8,7 @@ import createConnection from '../typeorm'
 import '../../container'
 import { AppError } from '../../errors/AppError'
 import "dotenv/config"
+import upload from '../../../config/upload'
 
 createConnection()
 const app = express()
@@ -15,6 +16,9 @@ const app = express()
 app.use(express.json())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
 app.use(router)
 
