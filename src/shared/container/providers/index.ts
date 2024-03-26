88d905cdfaa0c65087792da27d640c7897679330
sapiens19 <sapiens19@pop-os.localdomain>
2024-03-26 10:34:18 -0300

    Modified send email

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,28 +1,3 @@
-import { container } from 'tsyringe'
-import { DayjsDateProvider } from './DateProvider/implementations/DayjsDateProvider'
-import { IDateProvider } from './DateProvider/IDateProvider'
-import { IMailProvider } from './MailProvider/IMailProvider'
-import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider'
-import { LocalStorageProvider } from './StorageProvider/implementations/LocalStoregeProvider'
-import { IStorageProvider } from './StorageProvider/IStorageProvider'
-import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider'
-
-container.registerSingleton<IDateProvider>(
-  'DayjsDateProvider',
-  DayjsDateProvider,
-)
-
-container.registerInstance<IMailProvider>(
-  'EtherealMailProvider',
-  new EtherealMailProvider(),
-)
-
-const diskStorage = {
-  local: LocalStorageProvider,
-  s3: S3StorageProvider
-}
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  diskStorage[process.env.disk]
-)
+import "./DateProvider"
+import "./MailProvider"
+import "./StorageProvider"
