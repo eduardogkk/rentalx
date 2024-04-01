e89a6c07b16f5acc288c8cf1db13f07315cb1164
sapiens19 <sapiens19@pop-os.localdomain>
2024-03-31 22:42:01 -0300

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,6 +9,7 @@ import '../../container'
 import { AppError } from '../../errors/AppError'
 import "dotenv/config"
 import upload from '../../../config/upload'
+import cors from "cors"
 
 createConnection()
 const app = express()
@@ -20,6 +21,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
+app.use(cors())
 app.use(router)
 
 app.use(
